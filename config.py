770f839c529dfa53dd6c83c7333f665008a6b5e0
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-05-18 10:09:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ class Config(object):
 
 
     # Get from my.telegram.org (or @UseTGXBot)
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("APP_ID", "21740783"))
+    API_HASH = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6299192020").split())
     
